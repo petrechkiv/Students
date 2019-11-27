40b43462cd4d785eee347a678ec0bc993822eeee
Alex Leonenko <sanekleonenko@gmail.com>
2019-11-27 21:10:46 +0300

    Исправлена ошибка с добавление студента

--- views/teachers/_form.php
+++ views/teachers/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'role')->hiddenInput(['value' => 'teacher']) ?>
+    <?= $form->field($model, 'role')->hiddenInput(['value' => 'teacher'])->label('') ?>
 
     <?= $form->field($model, 'fio')->textInput(['maxlength' => true]) ?>
 
